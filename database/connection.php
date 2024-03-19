3d577b4d0e199ad178a1fe008272c6da5f1b0432
Juansantoss07 <juansantosscunha@gmail.com>
2024-03-19 16:48:02 -0300

    atualizado arquivo de banco de dados

--- database/connection.php
+++ database/connection.php
@@ -1,18 +1,17 @@
 <?php
 // Credenciais do banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "LoginCadastroPHP";
+$host = "ceu9lmqblp8t3q.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+$port = "5432";
+$database = "d57sibo83s99hv";
+$user = "uaaom582fhp077";
+$password = "p5fa730e03ef7237f20cff8dccc9211fc058cddfb7af533a3b7cd20aaa70a3fa2";
 
 // Conectar ao banco de dados
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = pg_connect("host=$host port=$port dbname=$database user=$user password=$password");
 
 // Verificar conexão
-if ($conn->connect_error) {
-    die("Erro na conexão: " . $conn->connect_error);
+if (!$conn) {
+    die("Erro na conexão: " . pg_last_error());
 }
-error_log('Conexão com o banco realizada com sucesso!')
+error_log('Conexão com o banco realizada com sucesso!');
 ?>
-
-
